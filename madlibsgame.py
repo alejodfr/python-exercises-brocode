6ad449a2cb8e5597a3bebfded4bbff2a406d7141
Alejo <alejodfr@gmail.com>
2026-02-09 07:20:13 -0600

    Initial commit: python exercises

---
+++ madlibsgame.py
@@ -0,0 +1,14 @@
+# Madlibs game
+# Word gae where you create a story
+# by filling in blanks with random words
+
+adjective1 = input("Enter an adjective: (description)")
+noun1 = input("Enter a noun: (person, place, or thing)")
+adjective2 = input("Enter another adjective: (description)")
+verb1 = input("Enter a verb ending with 'ing': (action)")
+adjective3 = input("Enter a  adjective: (description)")
+
+print(f"Today I went to a {adjective1} zoo. ")
+print(f"In an exhibit, I saw a {noun1} ")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
